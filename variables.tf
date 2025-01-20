a759acce1489d20499ae650aed0634694821117e
Colin Johnson <colin@cloudavail.com>
2025-01-19 19:59:53 -0800

    explain why aws_bcmdataexports_export export name was chosen as a default

--- variables.tf
+++ variables.tf
@@ -3,6 +3,8 @@ variable "cost_and_usage_report_bucket" {
 }
 
 variable "bcmdataexports_export_name" {
-  type    = string
+  type = string
+  # note that we utilize the name "standard-data-export" as a default value as this is the default
+  # export type presented in the Billing and Cost Management -> Data Exports -> Create export page within the AWS Console
   default = "standard-data-export"
 }
